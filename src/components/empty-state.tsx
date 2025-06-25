643872c0263bd308b1fab3e4ebdad1d1768e81d8
Khan Tauhid <tauheedxd90@gmail.com>
2025-06-25 14:52:14 +0530

    12: agents data table

---
+++ src/components/empty-state.tsx
@@ -0,0 +1,22 @@
+import { AlertCircleIcon } from "lucide-react"
+import Image from "next/image"
+
+interface Props {
+    title: string
+    description: string
+}
+
+export const EmptyState = ({
+    title,
+    description
+}: Props) => {
+    return (
+        <div className="flex flex-col items-center justify-center">
+            <Image src="/empty.svg" alt="Empty" height={240} width={240} />
+            <div className="flex flex-col gap-y-6 max-w-md mx-auto text-center">
+                <h6 className="text-lg font-medium">{title}</h6>
+                <p className="text-sm text-muted-foreground">{description}</p>
+            </div>
+        </div>
+    )
+}
